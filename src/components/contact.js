80eba891f54a1693313e3d0729a8f74cc1584e2d
devin mitchell <devin.a.mitchell@gmail.com>
2021-09-17 12:39:46 -0600

    MVP done

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import './contact.css'
+
 
 export default function Contact() {
     const user = {
-        resume: '',         //link to your resume download (drag the pdf to portfolio_template/src/photos then copy the route ex "../photos/myresume.pdf")
+        resume: 'asdf',         //link to your resume download (drag the pdf to portfolio_template/src/photos then copy the route ex "../photos/myresume.pdf")
         linkedIn: '',       //link to your LinkedIn
         gitHub: ''          //link to your gitHub
     }
 
     return (
         <div id='Contact'>
-           {user.resume && <a className='user_cv' href={user.resume} download>CV</a>}
+           <a className={user.resume ? 'user_cv' : 'no_cv'} href={user.resume} download>CV</a>
            <div className='footer_links'>
                 <a href='/'>Home</a>
                 <a href='#projects'>Projects</a>
