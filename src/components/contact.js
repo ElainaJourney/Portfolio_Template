46381e683693c470e8b3395e959b257220e01a61
devin mitchell <devin.a.mitchell@gmail.com>
2021-09-17 12:46:00 -0600

    quick fix on contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,15 +4,17 @@ import './contact.css'
 
 export default function Contact() {
     const user = {
-        resume: 'asdf',         //link to your resume download (drag the pdf to portfolio_template/src/photos then copy the route ex "../photos/myresume.pdf")
+        resume: '',         //link to your resume download (drag the pdf to portfolio_template/src/photos then copy the route ex "../photos/myresume.pdf")
         linkedIn: '',       //link to your LinkedIn
         gitHub: ''          //link to your gitHub
     }
 
     return (
         <div id='Contact'>
-           <a className={user.resume ? 'user_cv' : 'no_cv'} href={user.resume} download>CV</a>
-           <div className='footer_links'>
+            <div className='cv_wrapper'>
+                {user.resume && <a href={user.resume} download>CV</a>}
+            </div>
+            <div className='footer_links'>
                 <a href='/'>Home</a>
                 <a href='#projects'>Projects</a>
                 <a href='#about'>About</a>
