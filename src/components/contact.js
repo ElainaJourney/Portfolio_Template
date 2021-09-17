d2fba5b5dd642f21fb8a3dd27680e5f354b6c2b4
devin mitchell <devin.a.mitchell@gmail.com>
2021-09-17 15:53:55 -0600

    contact query

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,7 @@ import './contact.css'
 
 export default function Contact() {
     const user = {
-        resume: '',         //link to your resume download (drag the pdf to portfolio_template/src/photos then copy the route ex "../photos/myresume.pdf")
+        resume: 'asdf',         //link to your resume download (drag the pdf to portfolio_template/src/photos then copy the route ex "../photos/myresume.pdf")
         linkedIn: '',       //link to your LinkedIn
         gitHub: ''          //link to your gitHub
     }
